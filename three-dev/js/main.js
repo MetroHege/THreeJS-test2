1ef6f92d7e71ecd3b1daacf56d83fe4fb83b7008
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-11-18 09:57:47 +0200

    controllers

--- three-dev/js/main.js
+++ three-dev/js/main.js
@@ -3,8 +3,16 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { loadModels } from "./models.js";
 import { setupEnvironment } from "./environment.js";
 import { VRButton } from "three/addons/webxr/VRButton.js";
+import { XRControllerModelFactory } from "three/addons/webxr/XRControllerModelFactory.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 let camera, scene, renderer;
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
+let raycaster;
+const intersected = [];
+const tempMatrix = new THREE.Matrix4();
+let group;
 
 init();
 
@@ -51,6 +59,9 @@ function init() {
   camera.position.set(25, 10, 15);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
+  // Initialize VR controllers
+  initVR();
+
   // Set animation loop
   renderer.setAnimationLoop(function () {
     renderer.render(scene, camera);
@@ -63,3 +74,60 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+function initVR() {
+  // Controllers
+  controller1 = renderer.xr.getController(0);
+  controller1.addEventListener("selectstart", onSelectStart);
+  controller1.addEventListener("selectend", onSelectEnd);
+  scene.add(controller1);
+
+  controller2 = renderer.xr.getController(1);
+  controller2.addEventListener("selectstart", onSelectStart);
+  controller2.addEventListener("selectend", onSelectEnd);
+  scene.add(controller2);
+
+  const controllerModelFactory = new XRControllerModelFactory();
+
+  controllerGrip1 = renderer.xr.getControllerGrip(0);
+  controllerGrip1.add(
+    controllerModelFactory.createControllerModel(controllerGrip1)
+  );
+  scene.add(controllerGrip1);
+
+  controllerGrip2 = renderer.xr.getControllerGrip(1);
+  scene.add(controllerGrip2);
+
+  const geometry = new THREE.BufferGeometry().setFromPoints([
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, -1),
+  ]);
+  const line = new THREE.Line(geometry);
+  line.name = "line";
+  line.scale.z = 5;
+
+  controller1.add(line.clone());
+  controller2.add(line.clone());
+
+  raycaster = new THREE.Raycaster();
+
+  // Load and add custom model to controllerGrip2
+  const basePath = "assets/models/"; // Adjust the base path as needed
+  const loader = new GLTFLoader().setPath(basePath);
+  loader.load("gundy.gltf", function (gltf) {
+    gltf.scene.scale.set(0.0003, 0.0003, 0.0003);
+    let mymodel = gltf.scene;
+    mymodel.rotation.y = THREE.MathUtils.degToRad(180);
+    mymodel.rotation.x = THREE.MathUtils.degToRad(-36.5);
+    mymodel.position.set(0, 0.01, 0);
+    controllerGrip2.add(mymodel);
+  });
+}
+
+function onSelectStart(event) {
+  // Handle select start event
+}
+
+function onSelectEnd(event) {
+  // Handle select end event
+}
